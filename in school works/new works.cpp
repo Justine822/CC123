56a2f240a40ef495e7692944f0ec7c787861d5d8
Justine822 <xxhalkingxx@gmail.com>
2025-01-21 15:06:38 +0800

    Update new works.cpp

--- in school works/new works.cpp
+++ in school works/new works.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
 void fibo(int limit){
-	long long int num1=0,num2=1,temp;
+	long long int num1=0,num2=1,temp,total=0;
 	for (int i=0; i<limit; i++){
-	     if (num2 % 2 == 1)
+	    if (i % 2 == 0) {
+	        
+	        total+=num1;
+	    }
 	    cout<<num1<<endl;
-		temp = num1;
+	    temp = num1;
 		num2=num1+num2;
 		num1=num2;
 		num2=temp;
-		
-	}
+	   
+	}cout<< "the total of fibonannci sequence: "<<total;
 }
 int main() {
     int limit;
     cout<<"Enter limit: ";
     cin>>limit;
     fibo(limit);
-
     return 0;
 }
