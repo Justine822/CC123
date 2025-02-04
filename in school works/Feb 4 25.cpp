6cf4d545507014fc2960a3d35f6e037560588aa6
Justine822 <xxhalkingxx@gmail.com>
2025-02-04 13:20:25 +0800

    Create Feb 4 25.cpp

---
+++ in school works/Feb 4 25.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class shape{
+  protected:
+  double length, width, height;
+  public:
+  shape(double len, double wid, double hgt);//constructur
+  void getDimension();
+};
+
+shape::shape(double len, double wid, double hgt){
+  length=len;
+  width=wid;
+  height=hgt;
+};
+
+void shape::getDimension(){
+    cout<<"The length is: "<<length<<endl;
+    cout<<"The width is :"<<width<<endl;
+    cout<<"The height is: "<<height<<endl;
+}
+
+int main() {
+    shape smallBox(1,2,5);
+    smallBox.getDimension();
+    
+    return 0;
+}
