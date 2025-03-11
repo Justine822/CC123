79f350c285eb30f1fd211675af8bf1056f376d7e
Justine822 <xxhalkingxx@gmail.com>
2025-03-11 13:32:22 +0800

    Create mar 11 25.cpp

---
+++ in school works/mar 11 25.cpp
@@ -0,0 +1,12 @@
+
+using System;
+namespace HelloWorld{
+    class Program{
+    public static void Main(string[] args){
+        string test ="";
+        Console.WriteLine ("Hello world");
+        test = Console.ReadLine();
+        Console.WriteLine(test);
+        }
+    }
+}
