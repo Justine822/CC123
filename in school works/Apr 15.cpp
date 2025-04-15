20050846428d791f1c5bba5e7ab4799fefb4eff7
Justine822 <xxhalkingxx@gmail.com>
2025-04-15 14:23:50 +0800

    Update Apr 15.cpp

--- in school works/Apr 15.cpp
+++ in school works/Apr 15.cpp
@@ -1,30 +1,51 @@
 
 using System;
 
-public class HelloWorld
+ class HelloWorld
 {
-    static int Add(int num3){
+    public void classMethod(int num1, int num2)
+    {
+        console.WriteLine(num1 + num2);
+        console.WriteLine(num1 + "" + num2);
+        console.WriteLine($"{num1}{num2}");
+       
+    }
+    public MyClass(int classNo)
+    {
+         console.WriteLine("My Class" + classNo);
+    }
+    static int Add(int num3)
+    {
         int num1 = 10, num2 = 20;
         return num1 + num2 + num3;
     }
-    static double Divide (double x, double y){
+    static double Divide (double x, double y)
+    {
         return x / y;
     }
-    static int mycalc(int num){
+    static int mycalc(int num)
+    {
         int total = Add(num);
         return total;
     }
-    static int mycalc(int num1, int num2, int num3){
+    static int mycalc(int num1, int num2, int num3)
+    {
         return num1+num2+num3;
     }
-    static double mycalc(double num1, double num2){
+    static double mycalc(double num1, double num2)
+    {
         double total = Divide(x: num1, y: num2);
         return total;
     }
-    public static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine (mycalc(num1 : 18.5, num2: 0.8));
-        Console.WriteLine (mycalc(30));
-        Console.WriteLine (mycalc(num1: 10, num2: 20, num3: 6));
+        MyClass BS2AA = new MyClass(2);
+        BS2AA.classMethod(num1:10 , num2: 20);
+        
+        
+        
+        //Console.WriteLine (mycalc(num1 : 18.5, num2: 0.8));
+        //Console.WriteLine (mycalc(30));
+        //Console.WriteLine (mycalc(num1: 10, num2: 20, num3: 6));
     }
 }
