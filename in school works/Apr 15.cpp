00976b418a85d3ca8d5ff06d49cd829bebc9333e
Justine822 <xxhalkingxx@gmail.com>
2025-04-22 13:53:21 +0800

    Update Apr 15.cpp

--- in school works/Apr 15.cpp
+++ in school works/Apr 15.cpp
@@ -1,51 +1,29 @@
-
 using System;
 
- class HelloWorld
+class Shape
 {
-    public void classMethod(int num1, int num2)
+    private double rad =0, s1=0,s2=0;
+    public Shape()
     {
-        console.WriteLine(num1 + num2);
-        console.WriteLine(num1 + "" + num2);
-        console.WriteLine($"{num1}{num2}");
-       
+      Console.WriteLine("Shape Initialized");
     }
-    public MyClass(int classNo)
+    public Shape(double radius)
     {
-         console.WriteLine("My Class" + classNo);
+        rad=radius;
+        Console.WriteLine("Circle Shape Initialized radius"+rad);
     }
-    static int Add(int num3)
+    public Shape(double side1, double side2)
     {
-        int num1 = 10, num2 = 20;
-        return num1 + num2 + num3;
+    s1=side1;
+    s2=side2;
+    Console.WriteLine("Square Shape Initialized side1: "+s1+"side2 :"+s2);
     }
-    static double Divide (double x, double y)
-    {
-        return x / y;
-    }
-    static int mycalc(int num)
-    {
-        int total = Add(num);
-        return total;
-    }
-    static int mycalc(int num1, int num2, int num3)
-    {
-        return num1+num2+num3;
-    }
-    static double mycalc(double num1, double num2)
-    {
-        double total = Divide(x: num1, y: num2);
-        return total;
-    }
+}
+
+class Program 
+{
     static void Main()
     {
-        MyClass BS2AA = new MyClass(2);
-        BS2AA.classMethod(num1:10 , num2: 20);
-        
-        
-        
-        //Console.WriteLine (mycalc(num1 : 18.5, num2: 0.8));
-        //Console.WriteLine (mycalc(30));
-        //Console.WriteLine (mycalc(num1: 10, num2: 20, num3: 6));
+        Shape test_shape = new Shape(side1:3.8,side2:4.5);
     }
 }
