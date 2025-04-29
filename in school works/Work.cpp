2183897c285184bc7a14e617fb43fd331f14f89f
Justine822 <xxhalkingxx@gmail.com>
2025-04-29 21:47:42 +0800

    Update Work.cpp

--- in school works/Work.cpp
+++ in school works/Work.cpp
@@ -5,24 +5,33 @@ class Shelf
 {
     public string[] Books;
     public string[] Authors;
+    public string[] Books1;
+    public string[] Authors1;
     public string ShelfGenre;
 
     public Shelf(string[] books, string[] authors, string genre)
     {
         Books = books;
         Authors = authors;
+        Books1 = books;
+        Authors1 = authors;
         ShelfGenre = genre;
-    }
-
-    public void Details()
+    }    
+        public void Details()
     {
         Console.WriteLine($"\nGenre: {ShelfGenre}");
-        for (int i = 0; i < Books.Length; i++)
-        {
-            Console.WriteLine($"Author: {Authors[i]} - Book: {Books[i]}");
-        }
+        
+        Console.WriteLine($"Author: {Authors[0]} - Book: {Books[0]}");
+      
     }
+        public void Detaiils(){
+            Console.WriteLine($"\nGenre: {ShelfGenre}");
+            
+        Console.WriteLine($"Author {Authors1[1]} - Book: {Books[1]}");
+        }
+    
 }
+
 
 class Library
 {
@@ -35,33 +44,63 @@ class Library
 
     public void ShowLibraryDetails()
     {
-        foreach (var shelf in Shelves)
+        
+       foreach (var shelf in Shelves)
         {
-            shelf.Details();
+           shelf.Details();
+        }
+    }
+    public void ShowLibraryDetails1()
+    {
+        
+       foreach (var shelf in Shelves)
+        {
+           shelf.Detaiils();
         }
     }
 }
 
-class Program
+
+public class HelloWorld
 {
+    
     static void Main()
     {
-        // Create Horror Shelf
-        string[] horrorBooks = { "IT", "Dracula", "Frankenstein" };
-        string[] horrorAuthors = { "Stephen King", "Bram Stoker", "Mary Shelley" };
-        Shelf horrorShelf = new Shelf(horrorBooks, horrorAuthors, "Horror");
+    Console.WriteLine("What genre would you like horror or thriller?");
+    Console.Write(": ");
+    string ans = Console.ReadLine();
+    
+    
+    if(ans==("horror"))
+    {
+    string[] horrorAuthors={"Harlan Ellison","Robert Louis Stevenson"};
+    
+    string[] horrorBooks={"I have no mouth i must scream","Dr Jekyll and Mr Hyde"};
 
-        // Create Romance Shelf
-        string[] romanceBooks = { "Pride and Prejudice", "Me Before You", "The Notebook" };
-        string[] romanceAuthors = { "Jane Austen", "Jojo Moyes", "Nicholas Sparks" };
-        Shelf romanceShelf = new Shelf(romanceBooks, romanceAuthors, "Romance");
-
-        // Create Library and add shelves
+     Shelf horrorShelf = new Shelf(horrorBooks, horrorAuthors, "Horror");
+     
+    Library library = new Library();
+    library.AddShelf(horrorShelf);
+    library.ShowLibraryDetails();
+    } 
+    else if(ans==("thriller"))
+    {
+    string[] thrillerAuthors1={"Harlan Ellison","Robert Louis Stevenson"};
+    
+    string[] thrillerBooks1={"I have no mouth i must scream","Dr Jekyll and Mr Hyde"};
+    
+    Shelf thrillerShelf = new Shelf(thrillerBooks1, thrillerAuthors1, "Thriller");
+    
         Library library = new Library();
-        library.AddShelf(horrorShelf);
-        library.AddShelf(romanceShelf);
-
-        // Display all shelf details
-        library.ShowLibraryDetails();
+        library.AddShelf(thrillerShelf);
+        library.ShowLibraryDetails1();
+    }
+        
+        
+        
+        
+        
+        
+        
     }
 }
